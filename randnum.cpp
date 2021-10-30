f3629bc3e809a964ee7baa6d2d6d5e2a41c4a182
Fzuzhy <1305859509@qq,com>
2021-10-30 10:01:12 +0800

    111

---
+++ randnum.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int grade1_num()
+{
+	srand((unsigned)time(0)+(unsigned)rand());
+	int a=rand()%100;
+	return a;
+}
+
+
+int grade2_num()
+{
+	srand((unsigned)time(0)+(unsigned)rand());
+	int a=rand()%10000;
+	return a;
+}
+
+double grade3_num()
+{
+	srand((unsigned)time(0)+(unsigned)rand());
+	int a=rand()%10001; 
+	double b=(double)a/10000.0;
+	srand(time(NULL));
+	int c=rand()%10000;
+	b+=(double)c;
+	return b;
+}
+
+char grade12_char()
+{
+	char s[2]={'+','-'};
+	srand((unsigned)time(0)+(unsigned)rand());
+	int a=rand()%2;
+	return s[a];
+}
+
+char grade3_char()
+{
+	char s[4]={'+','-','*','/'};
+	srand((unsigned)time(0)+(unsigned)rand());
+	int a=rand()%4;
+	return s[a];
+}
